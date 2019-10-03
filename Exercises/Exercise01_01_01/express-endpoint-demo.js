cddf13278c9393514bbb023eb4c95d5ae1c5c1ea
connor <cwebdale006@west-mec.org>
2019-10-03 13:30:27 -0700

    fixing bugs

---
+++ Exercises/Exercise01_01_01/express-endpoint-demo.js
@@ -0,0 +1,9 @@
+let express = require('express');
+let app = express();
+let port = 8080;
+
+// serving some static content 
+app.use(express.static(__dirname));
+
+// turning our node.js applicatoin into a server 
+app.listen(port, () => console.log('Server listening on localhost:%s', port););
