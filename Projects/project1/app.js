286569c24b17cfb3019448fca345d0605af9442f
connor <cwebdale006@west-mec.org>
2019-10-08 15:27:30 -0700

    i have no idea what  i'm doing

--- Projects/project1/app.js
+++ Projects/project1/app.js
@@ -11,4 +11,20 @@ app.get('/', (req, res) => {
     res.render('index', { title: 'testing'})
 });
 
+app.use('/gible', function (req, res) {
+    res.send("<script src='pokemon2.js'>console.log('hello')</script>")
+});
+
+// app.use('/cubone', function (req, res) {
+//     res.send('')
+// });
+
+// app.use('/gible', function (req, res) {
+//     res.send('')
+// });
+
+// app.use('/gible', function (req, res) {
+//     res.send('')
+// });
+
 app.listen(port, () => console.log(`Successfully ran code`))
